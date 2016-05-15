3da0c602d3d661ca65008b20da02cd32dc5cb12a
Jacob <jacob.varner14@gmail.com>
2016-05-14 21:31:23 -0500

    Added a 'lastUpdate' JavaScript function to prevent updating the date on every page.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,8 @@ function changeImage(element) {
   document.getElementById("image-main").src = oldSource;
   element.src = newSource;
 }
+
+function lastUpdate() {
+  //Update here and it will autofill everywhere.
+  document.write("May 14, 2016");
+}
