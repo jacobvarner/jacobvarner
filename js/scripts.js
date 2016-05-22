d0bc6dff786b9fc7f32400d7de070b2a5ffaae3e
Jacob <jacob.varner14@gmail.com>
2016-05-21 21:09:38 -0500

    Added a mobile menu that works except the mobile menu stays open is you resize the window.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,17 @@ function lastUpdate() {
   //Update here and it will autofill everywhere.
   document.write("May 14, 2016");
 }
+
+function toggleMenu() {
+  var menuStatus = document.getElementById("menu-button").className;
+
+  if (menuStatus === "closed") {
+    document.getElementById("mobile-nav").style.display = "block";
+    document.getElementById("menu-icon").src = "images/menu-icon-close.png";
+    document.getElementById("menu-button").className = "open";
+  } else {
+    document.getElementById("mobile-nav").style.display = "none";
+    document.getElementById("menu-icon").src = "images/menu-icon.png";
+    document.getElementById("menu-button").className = "closed";
+  }
+}
